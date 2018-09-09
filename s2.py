7fadbbf802f50e1ebffbad9562faab8c4f7e4b3c
John Berthels <john.berthels@gmail.com>
2018-09-09 17:23:13 +0100

    S2C10

--- s2.py
+++ s2.py
@@ -2,6 +2,7 @@
 from Crypto.Cipher import AES
 import itertools
 from util import *
+from s1 import xor_buf
 
 def main():
     c10()
@@ -18,15 +19,51 @@ def c10():
     print("Re-encode matches? : {}".format(recipher_text == cipher_text))
 
 
+def aes128_cbc_encode(key, iv, plain_text):
+    ecb_cipher = AES.new(key, AES.MODE_ECB)
+    block_size = ecb_cipher.block_size
+    if len(plain_text) % block_size != 0:
+        raise RuntimeError("CBC requires padding to block size")
+    if len(iv) != block_size:
+        raise RuntimeError("IV must be one block")
+
+    plain_chunks = chunk(plain_text, block_size)
+
+    last_cipher_chunk = iv
+    cipher_chunks = []
+    for pc in plain_chunks:
+        next_cipher_chunk = ecb_cipher.encrypt(xor_buf(pc, last_cipher_chunk))
+        cipher_chunks.append(next_cipher_chunk)
+        last_cipher_chunk = next_cipher_chunk
+
+    return b''.join(cipher_chunks)
+
 def aes128_cbc_decode(key, iv, cipher_text):
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return cipher.decrypt(cipher_text)
+    ecb_cipher = AES.new(key, AES.MODE_ECB)
+    block_size = ecb_cipher.block_size
+    if len(cipher_text) % block_size != 0:
+        raise RuntimeError("CBC requires padding to block size")
+    if len(iv) != block_size:
+        raise RuntimeError("IV must be one block")
+
+    cipher_chunks = chunk(cipher_text, block_size)
+
+    last_cipher_chunk = iv
+    plain_chunks = []
+    for cc in cipher_chunks:
+        next_plain_chunk = xor_buf(last_cipher_chunk, ecb_cipher.decrypt(cc))
+        plain_chunks.append(next_plain_chunk)
+        last_cipher_chunk = cc
+
+    return b''.join(plain_chunks)
 
 
-def aes128_cbc_encode(key, iv, cipher_text):
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return cipher.encrypt(cipher_text)
-
+def c9():
+    block_size = 20
+    msg = b"YELLOW SUBMARINE"
+    padded_message = pkcs7_pad(msg, block_size)
+    expected_padded_message = b"YELLOW SUBMARINE\x04\x04\x04\x04"
+    print("S2C9 padded message correct: {}", padded_message == expected_padded_message)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,6 +1,23 @@
-__all__ = [ 'transpose', 'hex2bytes', 'chunk', 'slurp_base64_file', 'slurp_hex_file_as_lines' ]
+__all__ = [ 'pkcs7_pad', 'pkcs7_unpad', 'transpose', 'hex2bytes', 'chunk', 'slurp_base64_file', 'slurp_hex_file_as_lines' ]
 
 from base64 import b64decode
+import itertools
+
+def pkcs7_unpad(buf, block_size):
+    raise RuntimeError("pkcs7_unpad: bad padding")
+
+def pkcs7_pad(buf, block_size):
+    chunks = chunk(buf, block_size)
+    last_chunk = chunks[-1]
+    missing = block_size - len(last_chunk)
+    if missing == 0:
+        missing = block_size
+
+    addition = bytes(itertools.repeat(missing, missing)) 
+    chunks.append(addition)
+
+    return b''.join(chunks)
+
 
 def hex2bytes(hex_str):
     s = "".join(hex_str.split())
